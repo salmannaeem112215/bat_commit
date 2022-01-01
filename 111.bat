8fe4c095932cc1423b3a2b6e3953a76655f8a2dc
Salman-Laptop <salman.naeem.112215@gmail.com>
2022-01-01 05:00:01 +0500

    Update README with date 2022-01-01

---
+++ 111.bat
@@ -0,0 +1,2 @@
+@echo off
+cd /d "C:\SALMAN\flutter\bat_commit"
--- gits.bat
+++ gits.bat
@@ -1,6 +1,6 @@
 @echo off
 setlocal EnableDelayedExpansion
-
+cd /d "C:\SALMAN\flutter\bat_commit"
 REM Example usage:
 call :print_date_range "2022-01-01" "2023-01-10"
 exit /b
