3d16e43ae5e195f43a345462cbfacd58fe212085
Salman <101444405+salmannaeem112215@users.noreply.github.com>
2024-03-21 03:02:47 +0100

    Update README with date 2024-03-15

---
+++ 3.bat
@@ -0,0 +1,11 @@
+ @echo off
+setlocal EnableDelayedExpansion
+cd /d "C:\SALMAN\flutter\bat_commit"
+ for /l %%i in (1, 1, 1000000) do (
+                REM Append current date to README.md
+                echo "5" >> README.md
+
+                REM Commit changes to Git repository
+                git add .
+                git commit -m "Update README with date %%y-!mm!-!dd!"
+           )
